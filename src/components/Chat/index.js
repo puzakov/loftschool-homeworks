a7ac428eabb88541c4673f7a59e473f7ee7966b8
Artem Puzakov <apuzakov@awardwallet.com>
2019-04-09 15:09:54 +0500

    homework-2 done

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -1,1 +1,52 @@
-export { default } from './Chat';
+import React, { Component } from 'react';
+import Message from '../Message';
+import './Chat.css';
+
+class Chat extends Component {
+  state = {
+    messages: [],
+    messageInput: ''
+  };
+
+  changeInputMessage = event => {
+    this.setState({ messageInput: event.target.value });
+  };
+
+  sendMessageOnEnter = event => {
+    if (event.key === 'Enter') {
+      const { messages } = this.state;
+      messages.push({ text: this.state.messageInput });
+
+      this.setState(
+        {
+          messages: messages
+        },
+        () => this.setState({ messageInput: '' })
+      );
+    }
+  };
+
+  render() {
+    return (
+      <div className="chat">
+        <div className="message-list">
+          <div className="messages">
+            {this.state.messages.length > 0 &&
+              this.state.messages.map((item, index) => (
+                <Message text={item.text} key={index} />
+              ))}
+          </div>
+        </div>
+        <input
+          className="input-message"
+          value={this.state.messageInput}
+          onChange={this.changeInputMessage}
+          onKeyPress={this.sendMessageOnEnter}
+          type="text"
+        />
+      </div>
+    );
+  }
+}
+
+export default Chat;
