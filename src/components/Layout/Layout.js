335cea62aa9ff232a5ca273fd6c8d42651751be5
Artem Puzakov <apuzakov@awardwallet.com>
2019-04-18 11:09:57 +0500

    hw 4 done with 2 broken tests

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,18 +1,84 @@
-import React, { Fragment, PureComponent } from 'react';
+import React, { Fragment, Component, PureComponent } from 'react';
 import SectionTitle from '../SectionTitle';
+import LoginForm from '../LoginForm';
+import { AuthConsumer } from '../../contexts/Auth';
+import PropTypes from 'prop-types';
 import './Layout.css';
 
+const CongratsNode = () => {
+  return (
+    <p className="t-congratulation">
+      <span role="img" aria-label="congratulations!">
+        🎉
+      </span>
+      Поздравляем!{' '}
+      <span role="img" aria-label="congratulations!">
+        🎉
+      </span>
+      <br /> Вы вошли в систему!
+    </p>
+  );
+};
+
 class Layout extends PureComponent {
+  static propTypes = {
+    header: PropTypes.func,
+    footer: PropTypes.func
+  };
+
   render() {
-    return 'empty';
+    const { header, footer } = this.props;
+    let mainBlokClass = 'main';
+    mainBlokClass += header ? ' main--with-header' : '';
+    mainBlokClass += footer ? ' main--with-footer' : '';
+    const Header = () => this.renderHeader(header);
+    const Footer = () => this.renderFooter(footer);
+
+    return (
+      <Fragment>
+        {header && <Header />}
+        <main className={mainBlokClass}>
+          <SectionTitle className="main__title">Main</SectionTitle>
+          <AuthConsumer>
+            {authItems => {
+              return authItems.isAuthorized ? (
+                <CongratsNode />
+              ) : (
+                <LoginForm {...authItems} />
+              );
+            }}
+          </AuthConsumer>
+        </main>
+        {footer && <Footer />}
+      </Fragment>
+    );
   }
 
   renderHeader(HeaderChild) {
-    return 'empty';
+    // return class extends Component {
+      // render() {
+        console.log('Rendering HEADER');
+        return (
+          <header className="header">
+            <SectionTitle className="header__title">Header</SectionTitle>
+            <HeaderChild {...this.props} />
+          </header>
+        );
+    //   }
+    // };
   }
 
   renderFooter(FooterChild) {
-    return 'empty';
+    // return class extends Component {
+    //   render() {
+        return (
+          <footer className="footer">
+            <SectionTitle className="header__title">Footer</SectionTitle>
+            <FooterChild {...this.props} />
+          </footer>
+        );
+    //   }
+    // };
   }
 }
 
--- src/contexts/Auth/Auth.js
+++ src/contexts/Auth/Auth.js
@@ -2,10 +2,46 @@ import React, { PureComponent } from 'react';
 
 const { Provider, Consumer: AuthConsumer } = React.createContext('');
 
+const successCredentials = {
+  email: 'stu@dent.com',
+  password: '123'
+};
+
 class AuthProvider extends PureComponent {
+  state = {
+    isAuthorized: false,
+    email: '',
+    authorizeError: '',
+  };
+
+  getProviderValue = () => {
+    return {
+      ...this.state,
+      authorize: this.authorize,
+      logout: this.logout,
+    }
+  };
+
+  authorize = (email, password) => {
+    if (email === successCredentials.email && password === successCredentials.password) { 
+      this.setState({
+        isAuthorized: true,
+        email: email,
+        authorizeError: ''
+      });
+      return;
+    }
+
+    this.setState({authorizeError: 'Email или пароль введён не верно'});
+  };
+
+  logout = () => {
+    this.setState({ isAuthorized: false });
+  };
+
   render() {
     const { children } = this.props;
-    return <Provider>{children}</Provider>;
+    return <Provider value={this.getProviderValue()}>{children}</Provider>;
   }
 }
 
