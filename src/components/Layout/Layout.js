c33bc7e8a410fa954295300476c6e7b56a881aff
Artem Puzakov <apuzakov@awardwallet.com>
2019-04-18 18:56:44 +0500

    hw 4 done

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React, { Fragment, Component, PureComponent } from 'react';
+import React, { Fragment, PureComponent } from 'react';
 import SectionTitle from '../SectionTitle';
 import PropTypes from 'prop-types';
 import './Layout.css';
@@ -11,49 +11,36 @@ class Layout extends PureComponent {
 
   render() {
     const { header, footer, children } = this.props;
-    let mainBlokClass = 'main';
-    mainBlokClass += header ? ' main--with-header' : '';
-    mainBlokClass += footer ? ' main--with-footer' : '';
-    const Header = this.renderHeader(header);
-    const Footer = this.renderFooter(footer);
+    const mainBlokClass = `main ${header && 'main--with-header'} ${footer && 'main--with-footer'}`;
 
     return (
       <Fragment>
-        {header && <Header />}
+        {header && this.renderHeader(header)}
         <main className={mainBlokClass}>
           <SectionTitle className="main__title">Main</SectionTitle>
           {children}
         </main>
-        {footer && <Footer />}
+        {footer && this.renderFooter(footer)}
       </Fragment>
     );
   }
 
   renderHeader(HeaderChild) {
-    return class extends Component {
-      render() {
-        console.log('Rendering HEADER');
-        return (
-          <header className="header">
-            <SectionTitle className="header__title">Header</SectionTitle>
-            <HeaderChild {...this.props} />
-          </header>
-        );
-      }
-    };
+    return (
+      <header className="header">
+        <SectionTitle className="header__title">Header</SectionTitle>
+        <HeaderChild {...this.props} />
+      </header>
+    );
   }
 
   renderFooter(FooterChild) {
-    return class extends Component {
-      render() {
-        return (
-          <footer className="footer">
-            <SectionTitle className="header__title">Footer</SectionTitle>
-            <FooterChild {...this.props} />
-          </footer>
-        );
-      }
-    };
+    return (
+      <footer className="footer">
+        <SectionTitle className="header__title">Footer</SectionTitle>
+        <FooterChild {...this.props} />
+      </footer>
+    );
   }
 }
 
