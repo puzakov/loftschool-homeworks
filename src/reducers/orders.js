f089b7fd502e115d4a5da95c9c1cd9bf6c558568
Artem Puzakov <apuzakov@awardwallet.com>
2019-04-30 01:13:11 +0500

    hw 7 done

--- src/reducers/orders.js
+++ src/reducers/orders.js
@@ -9,6 +9,56 @@ import { ADD_INGREDIENT } from '../actions/ingredients';
 
 export default (state = [], action) => {
   switch (action.type) {
+    case CREATE_NEW_ORDER:
+      const { id, recipe } = action.payload;
+      return [
+        ...state,
+        {
+          id,
+          recipe,
+          ingredients: [],
+          position: 'clients'
+        }
+      ];
+    case MOVE_ORDER_NEXT: {
+      const order = state.find(item => item.id === action.payload);
+      const positions = {
+        clients: 'conveyor_1',
+        conveyor_1: 'conveyor_2',
+        conveyor_2: 'conveyor_3',
+        conveyor_3: 'conveyor_4',
+        conveyor_4: 'finish'
+      };
+
+      if (order.position === 'conveyor_4') {
+        const isFinished = order.recipe.reduce(
+          (result, value) =>
+            result && order.ingredients.findIndex(item => item === value) > -1,
+          true
+        );
+        if (!isFinished) return state;
+      }
+      const newPosition = positions[order.position];
+      if (positions[order.position]) order.position = newPosition;
+      return [...state];
+    }
+    case MOVE_ORDER_BACK: {
+      const order = state.find(item => item.id === action.payload);
+      const positions = {
+        conveyor_2: 'conveyor_1',
+        conveyor_3: 'conveyor_2',
+        conveyor_4: 'conveyor_3',
+        finish: 'conveyor_4'
+      };
+      const newPosition = positions[order.position];
+      if (positions[order.position]) order.position = newPosition;
+      return [...state];
+    }
+    case ADD_INGREDIENT:
+      const { from, ingredient } = action.payload;
+      const order = state.find(item => item.position === from);
+      order.ingredients = [...order.ingredients, ingredient];
+      return [...state];
     default:
       return state;
   }
